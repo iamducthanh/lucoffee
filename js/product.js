49b03ca8c31d4ebbcaaae8b0935f8415a8ae2091
Cua <cua@iMac-cua-Cua.local>
2023-11-25 00:52:56 +0700

    up

---
+++ js/product.js
@@ -0,0 +1,235 @@
+let products = [
+    {
+        productName: 'Americano (ICE)',
+        productCost: '20.000',
+        categoryCode: 'italy',
+        image: 'americano.JPG'
+    },
+    {
+        productName: 'Cappucino (HOT)',
+        productCost: '35.000',
+        categoryCode: 'italy',
+        image: 'capuchino.PNG'
+    },
+    {
+        productName: 'Caramel macchiato (ICE)',
+        productCost: '40.000',
+        categoryCode: 'italy',
+        image: 'caramelmacchiato.png'
+    },
+    {
+        productName: 'Caramel macchiato (HOT)',
+        productCost: '40.000',
+        categoryCode: 'italy',
+        image: 'carameomacchiato.PNG'
+    }
+    ,
+    {
+        productName: 'Vanilla Macchiato',
+        productCost: '40.000',
+        categoryCode: 'italy',
+        image: 'vallinamacchiato.png'
+    },
+    {
+        productName: 'England Creamy',
+        productCost: '20.000',
+        categoryCode: 'italy',
+        image: 'endland.JPG'
+    }
+    ,
+    {
+        productName: 'Trà Đào Cam Sả',
+        productCost: '35.000',
+        categoryCode: 'tra',
+        image: 'daocamsa.png'
+    },
+    {
+        productName: 'Trà Nhài Táo Xanh',
+        productCost: '40.000',
+        categoryCode: 'tra',
+        image: 'nhaitaochanh.png'
+    },
+    {
+        productName: 'Trà Hibicus Dâu Mận Muối',
+        productCost: '30.000',
+        categoryCode: 'tra',
+        image: 'hibicusdaumanmuoi.PNG'
+    },
+    {
+        productName: 'Trà Sen Vàng Lá Nếp',
+        productCost: '40.000',
+        categoryCode: 'tra',
+        image: 'senvanglanep.png'
+    },
+    {
+        productName: 'Trà Sen Vàng Macchiato',
+        productCost: '35.000',
+        categoryCode: 'tra',
+        image: 'senvangmacchiato.png'
+    },
+    {
+        productName: 'Nước Ép Chanh Vàng Bạc Hà',
+        productCost: '30.000',
+        categoryCode: 'nuocep',
+        image: 'chanhvangbacha.JPG'
+    },
+    {
+        productName: 'Nước Ép Cam Dứa',
+        productCost: '30.000',
+        categoryCode: 'nuocep',
+        image: 'camdua.png'
+    },
+    {
+        productName: 'Nước Ép Dứa Ổi',
+        productCost: '30.000',
+        categoryCode: 'nuocep',
+        image: 'duaoi.png'
+    },
+    {
+        productName: 'Nước Ép Dưa Hấu Quế',
+        productCost: '30.000',
+        categoryCode: 'nuocep',
+        image: 'duahauque.png'
+    },
+    {
+        productName: 'Nước Ép Táo Chanh',
+        productCost: '40.000',
+        categoryCode: 'nuocep',
+        image: 'nuoceptaochanh.png'
+    },
+    {
+        productName: 'Trà Ô Long Sen Sữa',
+        productCost: '30.000',
+        categoryCode: 'trasua',
+        image: 'olongsensua.JPG'
+    },
+    {
+        productName: 'Trà Sữa Tinh Than Tre Kem Trứng Dừa Nướng',
+        productCost: '40.000',
+        categoryCode: 'trasua',
+        image: 'thantretrung.JPG'
+    },
+    {
+        productName: 'Sữa Tươi Trân Châu Đường Đen Kem Trứng Dừa Nướng',
+        productCost: '40.000',
+        categoryCode: 'trasua',
+        image: 'chanchaukemtrung.JPG'
+    },
+    {
+        productName: 'Trà Gạo Rang Macchiato',
+        productCost: '30.000',
+        categoryCode: 'trasua',
+        image: 'gaorangmachiato.png'
+    },
+    {
+        productName: 'Bạc Sỉu',
+        productCost: '25.000',
+        categoryCode: 'vietnam',
+        image: 'bacsiu.png'
+    },
+    {
+        productName: 'Cafe Kem Muối',
+        productCost: '30.000',
+        categoryCode: 'vietnam',
+        image: 'cafekemmuoi.PNG'
+    },
+    {
+        productName: 'Cafe Hạnh Nhân',
+        productCost: '35.000',
+        categoryCode: 'vietnam',
+        image: 'hanhnhan.png'
+    },
+    {
+        productName: 'Phindi Choco',
+        productCost: '30.000',
+        categoryCode: 'vietnam',
+        image: 'phindichoco.JPG'
+    },
+    {
+        productName: 'Cafe Nâu',
+        productCost: '15.000',
+        categoryCode: 'vietnam',
+        image: 'americano.JPG'
+    },
+    {
+        productName: 'Cafe Đen',
+        productCost: '15.000',
+        categoryCode: 'vietnam',
+        image: 'americano.JPG'
+    },
+    {
+        productName: 'Sinh Tố Bơ Hạnh Nhân',
+        productCost: '30.000',
+        categoryCode: 'sinhto',
+        image: 'bahanhnhan.JPG'
+    },
+    {
+        productName: 'Sinh Tố Xoài Đậu Phộng',
+        productCost: '30.000',
+        categoryCode: 'sinhto',
+        image: 'sinhtoxoaidauphong.PNG'
+    },
+    {
+        productName: 'Sinh Tố Xoài Dâu Đào',
+        productCost: '35.000',
+        categoryCode: 'sinhto',
+        image: 'xoaidaudao.JPG'
+    },
+    {
+        productName: 'Sinh Tố Sữa Chua Dâu Tây',
+        productCost: '35.000',
+        categoryCode: 'sinhto',
+        image: 'suachuadautay.png'
+    },
+    {
+        productName: 'Sinh Tố Sữa Chua Việt Quất',
+        productCost: '35.000',
+        categoryCode: 'sinhto',
+        image: 'suachuavietquat.png'
+    },
+    {
+        productName: 'Sữa Chua Đào Kem Cheese',
+        productCost: '35.000',
+        categoryCode: 'daxay',
+        image: 'suachuadaokemche.JPG'
+    },
+    {
+        productName: 'Coffee Caramel Đá Xay',
+        productCost: '40.000',
+        categoryCode: 'daxay',
+        image: 'carameldaxay.JPG'
+    },
+    {
+        productName: 'Bạc Hà Chocolate Đá Xay',
+        productCost: '35.000',
+        categoryCode: 'daxay',
+        image: 'bachachocolate.JPG'
+    }
+]
+
+let containerProduct = document.getElementById('container_product');
+
+containerProduct.innerHTML = '';
+
+products.forEach(product => {
+    containerProduct.innerHTML += '<div class="col-sm-6 col-lg-4 all '+product.categoryCode+'">'+
+'<div class="box">'+
+  '<div style="background-color: pink;">'+
+    '<div class="img-box" style="height: 400px; padding: 0px; background-color: beige;">'+
+      '<img src="images/'+product.image+' " alt="">'+
+    '</div>'+
+    '<div class="detail-box" style="background-color: pink; color: #8e0f0f">'+
+      '<h6>'+
+      product.productName+
+      '</h6>'+
+      '<div class="options">'+
+        '<h5><b>'+
+        product.productCost+
+        '</b></h5>'+
+      '</div>'+
+    '</div>'+
+  '</div>'+
+'</div>'+
+'</div>';
+});
+
